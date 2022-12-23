5a7820bf1aeeec2e2c5dbcaddf7c885b4dbf508d
Tatiana Arango Cardona <tatianarangoc@gmail.com>
2022-12-23 11:11:16 +0100

    database created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const Database = require("better-sqlite3")
 // create and config server
 const server = express();
 server.use(cors());
@@ -12,5 +12,15 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
+server.get('/movie/:movieId', (req, res) => { 
+  console.log('URL  params:', req.params);
+  console.log('URL param movieId:', req.params.movieId)
+  
+const movie = movie.find(movie => movie.id === req.params.movieId)
+  console.log('movie', movie)});
+
 const staticServerPathWeb = './src/public-react';
 server.use(express.static(staticServerPathWeb));
+
+const db = new Database('./src/db/database.db', { ... });
+
