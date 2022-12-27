5bb62f50fd7539d971bd3b6743c2a29a5f6cfc67
SaraRivAl <sari_ra@hotmail.com>
2022-12-27 11:17:34 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const Database = require("better-sqlite3")
+const movies = require('./data/movies.json');
+const Database = require('better-sqlite3');
 // create and config server
 const server = express();
 server.use(cors());
@@ -11,16 +12,16 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
-
-server.get('/movie/:movieId', (req, res) => { 
+//Buscar película por id
+server.get('/movie/:movieId', (req, res) => {
   console.log('URL  params:', req.params);
-  console.log('URL param movieId:', req.params.movieId)
-  
-const movie = movie.find(movie => movie.id === req.params.movieId)
-  console.log('movie', movie)});
+  console.log('URL param movieId:', req.params.movieId);
+
+  const movie = movie.find((movie) => movie.id === req.params.movieId);
+  console.log('movie', movie);
+});
 
 const staticServerPathWeb = './src/public-react';
 server.use(express.static(staticServerPathWeb));
 
-const db = new Database('./src/db/database.db', { ... });
-
+//const db = new Database('./src/db/database.db', { ... });
