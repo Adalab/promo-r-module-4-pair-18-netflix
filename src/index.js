0962b1ae1155e5e7621fcf807d0c3524beaace49
Tatiana Arango Cardona <tatianarangoc@gmail.com>
2022-12-27 19:21:59 +0100

    some changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const movies = require('./data/movies.json');
+//const movies = require('./data/movies.json');
 const Database = require('better-sqlite3');
+const db = new Database('./db/database.db', { verbose: console.log });
 // create and config server
-const server = express();
-server.use(cors());
-server.use(express.json());
+const server = express(); //creamos servidor, inicia el proceso de escuchar
+server.use(cors()); // configuramos el servidor para que sea una API pública, si quitamos cors será API privada
+server.use(express.json()); //configuramos el servidor le decimos q vamos a trabajar con Json
 
 // init express aplication
 const serverPort = 4000;
@@ -14,14 +15,24 @@ server.listen(serverPort, () => {
 });
 //Buscar película por id
 server.get('/movie/:movieId', (req, res) => {
-  console.log('URL  params:', req.params);
-  console.log('URL param movieId:', req.params.movieId);
-
-  const movie = movie.find((movie) => movie.id === req.params.movieId);
-  console.log('movie', movie);
+  console.log(req.query);
+  res.json({
+    success: 'true',
+    movies: { movies },
+  });
 });
 
 const staticServerPathWeb = './src/public-react';
 server.use(express.static(staticServerPathWeb));
 
+server.get('/movies', (req, res) => {
+  const query = db.prepare('SELECT * FROM movies'); //Preparar la query
+  const movies = query.all(); //ejecutar la query, devuelve un array con todos los datos que devuelve query
+  const response = {
+    success: true,
+    movies: movies,
+  };
+  console.log(movies);
+  res.json(response);
+});
 //const db = new Database('./src/db/database.db', { ... });
